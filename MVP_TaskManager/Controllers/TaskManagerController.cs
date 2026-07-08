ae10f894447a87c408ecf16ddc69f21ed519dfbb
Rosenshtein <73759229+Rosenshtein@users.noreply.github.com>
2026-07-08 18:21:32 +0300

    Версия 0.0.1. Доделаны задачи пользователей: 1) поиск задачи по id пользователя 2)

--- MVP_TaskManager/Controllers/TaskManagerController.cs
+++ MVP_TaskManager/Controllers/TaskManagerController.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using MVP_TaskManager.Models;
 
 namespace MVP_TaskManager.Controllers;
 
+
 [ApiController]
 [Route("api/users")]
-public class TaskManagerController : ControllerBase
+public class UsersController : ControllerBase
 {
     private static List<User> users = new();
     private static int nextId = 1;
+
 
     [HttpGet]
     public ActionResult<List<User>> GetAll()
     {
         return users;
     }
+
 
     [HttpGet("{id}")]
     public ActionResult<User> GetById(int id)
@@ -26,11 +30,52 @@ public class TaskManagerController : ControllerBase
         return user;
     }
 
+
     [HttpPost]
-    public ActionResult<User> Create(User user)
+    public ActionResult<User> CreateUser(User user)
     {
         user.Id = nextId++;
         users.Add(user);
         return user;
+    }
+}
+
+[ApiController]
+[Route("api/tasks")]
+public class TasksController : ControllerBase
+{
+    private static List<User> users = new();
+    private static List<Tasks> tasks = new();
+    private static int nextId = 1;
+
+    [HttpGet("{id_user}")] //поиск задач у определенного пользователя
+    public ActionResult<List<Tasks>> GetAllTaskByUser(int id_user)
+    {
+        //var task = tasks.FirstOrDefault(user_task => user_task.id_user == id_user);
+        var task = tasks.Where(user_task => user_task.id_user == id_user).ToList(); //это список
+        if (task == null)
+            return NotFound();
+
+        return task;
+    }
+
+    [HttpPost] // Создание задачи
+    public IActionResult CreateTask(Tasks task) // Метод принимает объект от пользователя в формате json, где есть все входные данные
+    {
+        try
+        {
+            tasks.Add(new Tasks
+            {
+                Id_task = nextId++,
+                Name_task = task.Name_task,
+                Description_task = task.Description_task,
+                id_user = task.id_user,
+            });
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return NotFound();
+        }
     }
 }
---
+++ MVP_TaskManager/Models/Tasks.cs
@@ -0,0 +1,10 @@
+﻿namespace MVP_TaskManager.Models
+{
+    public class Tasks
+    {
+        public int Id_task { get; set; }
+        public int id_user { get; set; }
+        public string Name_task { get; set; } = string.Empty;
+        public string Description_task { get; set; } = string.Empty;
+    }
+}
